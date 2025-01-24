ab0173f8d2fc685793d2073e4f3389e3a2c7d2de
yabiarium <yabiarium@gmail.com>
2025-01-24 15:09:00 +0900

    ConstItemの変数登録処理を作成

--- src/main/java/lang/c/parse/ConstItem.java
+++ src/main/java/lang/c/parse/ConstItem.java
@@ -5,6 +5,13 @@ import lang.c.*;
 
 public class ConstItem extends CParseRule {
 
+	int size;
+	String identName;
+	boolean isExistMult = false; // *があるか
+	boolean isExistAmp = false; // &があるか
+	boolean isArray = false; // 配列か
+	boolean isGlobal;
+
 	public ConstItem(CParseContext pcx) {
 		super("ConstItem");
 		setBNF("constItem ::= [ MULT ] IDENT ASSIGN [ AMP ] NUM"); //CV10~
@@ -17,19 +24,23 @@ public class ConstItem extends CParseRule {
 	public void parse(CParseContext pcx) throws FatalErrorException {
 		CTokenizer ct = pcx.getTokenizer();
 		CToken tk = ct.getCurrentToken(pcx);
+		CToken col = tk; //宣言済みの変数のエラー表示で使用する
 
 		try {
 			if(tk.getType() == CToken.TK_MULT){
-				tk = ct.getNextToken(pcx); // *を読んだことにする
+				tk = ct.getNextToken(pcx); // *を読み飛ばす
+				isExistMult = true;
 			}
 			
 			if(tk.getType() != CToken.TK_IDENT){
 				pcx.recoverableError(tk + " constItem: *の後ろは IDENT です"); //先頭がIDENTならisFirstでチェックしているのでここには来ないため、このエラーメッセージでよい
 			}
-	
-			tk = ct.getNextToken(pcx); // IDENTを読む
+			identName = tk.getText();
+			tk = ct.getNextToken(pcx); // IDENTを読み飛ばす
+
 			if(tk.getType() == CToken.TK_ASSIGN){
-				tk = ct.getNextToken(pcx); // =を読む
+				isArray = true;
+				tk = ct.getNextToken(pcx); // =を読み飛ばす
 			}else{
 				if(tk.getType() == CToken.TK_AMP || tk.getType() == CToken.TK_NUM){
 					pcx.warning(tk + " constItem: =を補いました");
@@ -39,15 +50,33 @@ public class ConstItem extends CParseRule {
 			}
 
 			if(tk.getType() == CToken.TK_AMP){
-				tk = ct.getNextToken(pcx); // &を読む
+				isExistAmp = true;
+				tk = ct.getNextToken(pcx); // &を読み飛ばす
 			}
-			if(tk.getType() != CToken.TK_NUM){
-				pcx.recoverableError(tk + "constItem: 定数の初期化がありません");
+			if(tk.getType() == CToken.TK_NUM){
+				size = Integer.valueOf(tk.getText());
+			}else{
+				pcx.recoverableError(tk + "constItem: 定数の初期化がありません");	
 			}
-			tk = ct.getNextToken(pcx); // NUMを読む
+			tk = ct.getNextToken(pcx); // NUMを読み飛ばす
 
 		} catch (RecoverableErrorException e) {
 			// 処理はint/constDeclに託す
+		}
+
+
+		// 変数登録
+		CSymbolTableEntry entry;
+		final boolean isConst = true;
+		size = 1;
+		if (isExistMult) {
+			entry = new CSymbolTableEntry(CType.getCType(CType.T_pint), size, isConst);
+		} else {
+			entry = new CSymbolTableEntry(CType.getCType(CType.T_int), size, isConst);
+		}
+
+		if ( !pcx.getSymbolTable().registerLocal(identName, entry) ) {
+			pcx.recoverableError(col + " 既に宣言されている変数です");
 		}
 	}
 
